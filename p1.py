cfaf14e958942eae8ec0e04dad14d355bd4b4014
Luana Barros <dev.luana@gmail.com>
2020-12-22 14:07:44 -0300

    tentando enviar mssg ao coord

--- p1.py
+++ p1.py
@@ -4,6 +4,37 @@ import socket
 import urllib.request
 import json
 import sys
+import signal
+from contextlib import contextmanager
+import time
+
+global end_time
+end_time = False
+
+@contextmanager
+def timeout(time):
+    # Register a function to raise a TimeoutError on the signal.
+    signal.signal(signal.SIGALRM, raise_timeout)
+    # Schedule the signal to be sent after ``time``.
+    signal.alarm(time)
+
+    try:
+        yield
+    except TimeoutError:
+        pass
+    finally:
+        # Unregister the signal so it won't be triggered
+        # if the timeout is not reached.
+        signal.signal(signal.SIGALRM, signal.SIG_IGN)
+
+
+def raise_timeout(signum, frame):
+    print('End of time')
+
+    global end_time
+    end_time = True
+
+    raise TimeoutError
 
 def local_time(counter):
     return ' (LAMPORT_TIME={})'.format(counter)
@@ -39,6 +70,32 @@ def recv_message(server_ip, counter, sock):
     print('Message received from {} at {}'.format(server_ip,local_time(counter)))
     return counter
 
+def get_permission(server_ip, sock):
+    file = 'bd.txt'
+    data = json.dumps({"GET":file})
+
+    
+    s.send(data.encode())
+    print('Trying to get permission...')
+
+    # Add a timeout block.
+    with timeout(5):
+        resp = s.recv(1024)
+        print(resp)
+        if (resp == "Denied"):
+            print('Denied')
+        if (resp == "OK"):
+            print('I am writing on the file...')
+
+
+
+def my_func():
+    # Add a timeout block.
+    with timeout(5):
+        print('entering block')
+        time.sleep(10)
+        print('This should never get printed because the line before timed out')
+
 if __name__ == '__main__': 
     HOST = sys.argv[1]
     PORT = 8888        # The port used by the server
@@ -46,8 +103,12 @@ if __name__ == '__main__':
     external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
     print('This IP address is: ', external_ip)
     
+    signal.signal(signal.SIGALRM, handler)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        counter = connect(counter, s, HOST, PORT)
+        s.connect((HOST, PORT))
+        get_permission(HOST, s)
+        
         # counter = send_message(HOST, counter, s)
         # counter = recv_message(HOST, counter, s)
         # counter = send_message(HOST, counter, s)
