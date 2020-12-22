bee42c9f94248f3c436ab7fbe12b17ddbb0ce280
Luana Barros <dev.luana@gmail.com>
2020-12-22 17:19:38 -0300

    receiving second by arg

--- p1.py
+++ p1.py
@@ -6,7 +6,7 @@ import json
 import sys
 import time
 
-def get_permission(server_ip, sock):
+def get_permission(server_ip, sock, sec):
 
     sock.send("GET".encode())
     print('Trying to get permission...')
@@ -16,9 +16,8 @@ def get_permission(server_ip, sock):
         print(resp)
         if (resp == "Allowed"):
             print('I am writing on the file...')
-            time.sleep(5)
+            time.sleep(sec)
             sock.send("FREE".encode('utf-8'))
-            time.sleep(10)
             sock.close()
             break
 
@@ -26,13 +25,14 @@ def get_permission(server_ip, sock):
 if __name__ == '__main__': 
     HOST = sys.argv[1]
     PORT = 8888        # The port used by the server
-    
+    second_with_file = sys.argv[2]
+
     external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
     print('This IP address is: ', external_ip)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        get_permission(HOST, s)
+        get_permission(HOST, s, second_with_file)
         print('Terminou')
         # counter = send_message(HOST, counter, s)
         # counter = recv_message(HOST, counter, s)
